4a3eb70c0a8804dacb32428d48b6c8985b05e611
HADMARINE <joonlee031@live.com>
2019-11-20 04:11:21 +0900

    Update Comsil.tsx
    testing now

--- src/routes/service/Comsil.tsx
+++ src/routes/service/Comsil.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import Product from './components/Product';
 const GlobalStyle = createGlobalStyle`
 * {
   margin: 0;
@@ -11,6 +12,12 @@ export default class Comsil extends Component {
     return (
       <>
         <GlobalStyle />
+        <Product
+          img="https://occ-0-988-993.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABSpHMNyvIRy6PolVWkmMg80JA0Kdcldq3vIxohYsRrtBKVZEyJAW6ls4b6EoKP7DzCQ6Fvnq5C694tN5eN06_Hb5gHp2.webp?r=105"
+          title="타이틀"
+          content="컨텐트ㅓ라이너라ㅣㅇ너ㅣㅏ렁니ㅏㅓㄹㄴ이ㅏㅓ린어리ㅏㅇ너리ㅏㄴ어리ㅏㅇ너ㅣ라ㅓㅇ니ㅏ렁니ㅏ러ㅣㅏㅇ너리나러ㅣ아너링너ㅏㄹ"
+          link="/"
+        />
         <div></div>
       </>
     );
