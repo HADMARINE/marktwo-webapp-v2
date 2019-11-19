292579c61b71dae14532928c07ce839c4d698bbf
HADMARINE <joonlee031@live.com>
2019-11-20 02:49:36 +0900

    notfound.tsx : create styled global

--- src/routes/util/notfound.tsx
+++ src/routes/util/notfound.tsx
@@ -1,11 +1,25 @@
 import React, { Component, Fragment } from 'react';
-
+import styled, { createGlobalStyle } from 'styled-components';
 import '../../styles/index.scss';
 
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  background-color:rgb(24,24,24);
+  color:white;
+}
+.form-control{
+  
+  background-color: rgb(60,60,60) !important;
+  color: white !important;
+  border-color: black !important;
+}`;
 class Notfound extends Component {
   render() {
     return (
       <>
+        <GlobalStyle />
         <div className="Style-center Style-overflow-hidden">
           <div>
             <p className="Module-notfoundmove" />
