2aee647650f856d4838345d88b0630a4d22595fe
HADMARINE <joonlee031@live.com>
2019-11-20 02:33:34 +0900

    register.tsx : added global state

--- src/routes/main/register.tsx
+++ src/routes/main/register.tsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import { Button, InputGroup, FormControl } from 'react-bootstrap';
 import apiUri from '../api/getApiUri';
-
 import '../../styles/index.scss';
+import styles, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  background-color:rgb(24,24,24);
+  color:white;
+}
+.form-control{
+  
+  background-color: rgb(60,60,60) !important;
+  color: white !important;
+  border-color: black !important;
+  :focus{
+    /* background-color:black; */
+  }
+
+}`;
 export default class Register extends Component {
   state = {
     id: '',
