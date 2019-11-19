8246a4e280b9114226e91214ed3ba0fec38eab04
HADMARINE <joonlee031@live.com>
2019-11-20 02:49:08 +0900

    Update Utility.tsx
    initial commit

---
+++ src/routes/service/Utility.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  background-color:rgb(24,24,24);
+  color:white;
+}
+.form-control{
+  
+  background-color: rgb(60,60,60) !important;
+  color: white !important;
+  border-color: black !important;
+  :focus{
+    /* background-color:black; */
+  }
+
+}`;
+export default class Utilty extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+      </>
+    );
+  }
+}
