0a4e2a9667519802578fa84fde18dbc558e56c1c
HADMARINE <joonlee031@live.com>
2019-11-20 11:05:35 +0900

    Update ProductInfoList.tsx
    1. exception
    2. default value
    3. default picture

--- src/routes/service/components/ProductInfoList.tsx
+++ src/routes/service/components/ProductInfoList.tsx
@@ -7,15 +7,32 @@ export default class ProductList extends Component<any> {
   };
 
   render() {
-    const data = JSON.parse(this.props.data);
-    const list = data.map((info: any) => {
-      <Product
-        key={info.id}
-        img={info.img}
-        content={info.content}
-        link={info.link}
-      />;
-    });
+    let data;
+    let list;
+    try {
+      data = JSON.parse(this.props.data);
+      list = data.map((info: any) => (
+        <Product
+          key={info.id}
+          title={info.title}
+          img={info.img}
+          content={info.content}
+          link={info.link}
+        />
+      ));
+    } catch (e) {
+      console.log(e);
+      list = (
+        <Product
+          key="0"
+          img="https://occ-0-988-993.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABSpHMNyvIRy6PolVWkmMg80JA0Kdcldq3vIxohYsRrtBKVZEyJAW6ls4b6EoKP7DzCQ6Fvnq5C694tN5eN06_Hb5gHp2.webp?r=105"
+          content="표시할 데이터가 없습니다."
+          title="데이터가 없습니다."
+          link="/"
+        />
+      );
+    }
+
     return <div>{list}</div>;
   }
 }
