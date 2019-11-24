4f063e2746370054f43e1937f7f6e2f4efc6ebf6
이호준 <46421618+HADMARINE@users.noreply.github.com>
2019-11-25 02:29:39 +0900

    change apiuri

--- src/routes/api/getApiUri.ts
+++ src/routes/api/getApiUri.ts
@@ -1,6 +1,6 @@
 export const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:4000'
-    : 'https://api.dodoli.net';
+    : 'https://api.marktwo.net';
 
 export default baseURL;
