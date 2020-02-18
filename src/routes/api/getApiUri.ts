3355b10765a85df02d9642ac506070055c719ab8
HADMARINE <tsx@kakao.com>
2020-02-19 00:34:13 +0900

    Update getApiUri.ts

--- src/routes/api/getApiUri.ts
+++ src/routes/api/getApiUri.ts
@@ -1,6 +1,6 @@
 export const baseURL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:4000'
+    ? 'http://localhost:60010'
     : 'https://api.marktwo.net';
 
 export default baseURL;
